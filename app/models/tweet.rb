7441c82c0a764a877e83c8193285029eb0dda72d
Lucas Poffo <ltkpoffo@gmail.com>
2019-04-28 14:16:02 -0300

    Change Tweet model to use custom id column to use the id given from their API.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,5 @@
 class Tweet < ApplicationRecord
+  self.primary_key = "id"
   belongs_to :user
   has_and_belongs_to_many :hashtags
 end
--- db/migrate/20190422000551_create_tweets.rb
+++ db/migrate/20190422000551_create_tweets.rb
@@ -1,11 +1,13 @@
 class CreateTweets < ActiveRecord::Migration[5.1]
   def change
-    create_table :tweets do |t|
+    create_table :tweets, id: false do |t|
+      t.integer :id, :limit => 8
       t.string :text
       t.references :user, foreign_key: true
       t.datetime :created_at
 
       t.timestamps
     end
+    add_index :tweets, :id, unique: true
   end
 end
