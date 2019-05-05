b3f9e615c54a61633f44027fb4d340a1afe90689
Lucas Poffo <ltkpoffo@gmail.com>
2019-05-05 19:51:11 -0300

    Restart TwitterStreamService when HashtagFilter changes

--- app/models/hashtag_filter.rb
+++ app/models/hashtag_filter.rb
@@ -1,3 +1,7 @@
 class HashtagFilter < ApplicationRecord
-  
+  after_commit :restart_stream_service, on: [:create, :destroy]
+
+  def restart_stream_service
+    TwitterStreamService.instance.filterByHashtags(HashtagFilter.all)
+  end
 end
