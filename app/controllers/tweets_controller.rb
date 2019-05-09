5cffd11d9662974f0d02ae4ce1426a56dcfe1682
Lucas Poffo <ltkpoffo@gmail.com>
2019-05-09 01:19:30 -0300

    Eagerload tweet user

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,12 @@ class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show]
 
   def index
-    @tweets = params[:hashtags] ? Tweet.contains_hashtags(params[:hashtags]) : Tweet.all
+    @tweets = params[:hashtags] ? Tweet.contains_hashtags(params[:hashtags]).includes(:user) : Tweet.all.includes(:user)
+    @tweets = @tweets.map do |tweet|
+      tweet.attributes.merge(
+        'user' => tweet.user
+      )
+    end
     paginate json: @tweets
   end
 
